58a5d71cbdfdd8ec1210f89351843999724076ae
AsmaaMoawad <asmaa_fcih@yahoo.com>
2017-05-03 18:44:32 +0200

    IA

---
+++ IA project 1/WebApplication2/Controllers/projectController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class projectController : Controller
+    {
+        // GET: project
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        //public ActionResult example()
+        //{
+        //    return View();
+        //}
+        public ActionResult Editor()
+        {
+            return View();
+        }
+        public ActionResult style()
+        {
+            return View();
+        }
+         public ActionResult Inbox()
+        {
+            return View();
+        }
+
+        public ActionResult schedule()
+         {
+             return View();
+         }
+
+        public ActionResult style1()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult wall()
+        {
+            return View();
+
+        }
+        
+
+        public ActionResult waitingarticle()
+        {
+            return View();
+        }
+
+        public ActionResult start()
+        {
+            return View();
+        }
+
+        public ActionResult user()
+        {
+            return View();
+        }
+    }
+}
